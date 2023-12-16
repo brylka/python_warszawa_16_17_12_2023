d9ec531501802c04eae6b8d2d48f9f11bc0433ae
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-16 09:34:53 +0100

    średnia wzrostu

--- dane.py
+++ dane.py
@@ -8,4 +8,11 @@ with open('dane.csv', 'r', encoding='utf-8') as file:
 data = [line.strip().split(',') for line in lines]
 
 #print(data[1:])
-print(data)
+#print(data)
+
+total_height = 0
+
+for row in data:
+    total_height += int(row[2])
+
+print(total_height/len(data))
