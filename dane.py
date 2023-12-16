09d2d89859dfcf971728d147b0be91633de7bec5
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-16 09:50:21 +0100

    średnia wzrostu Warszawa

--- dane.py
+++ dane.py
@@ -8,13 +8,19 @@ with open('dane.csv', 'r', encoding='utf-8') as file:
 data = [line.strip().split(',') for line in lines]
 
 #print(data[1:])
-#print(data)
+print(data)
 
-total_height = 0
-i = 0
+total_height_warszawa = 0
+total_height_poznan = 0
+i_warszawa = 0
+i_poznan = 0
 for row in data:
     if row[4] == 'Warszawa':
-        total_height += int(row[2])
-        i+=1
+        total_height_warszawa += int(row[2])
+        i_warszawa+=1
+    elif row[4] == 'Poznań':
+        total_height_poznan += int(row[2])
+        i_poznan+=1
 
-print(total_height/i)
+print(f"Średnia wzrostu dla Warszawy: {total_height_warszawa/i_warszawa}")
+print(f"Średnia wzrostu dla Poznania: {total_height_poznan/i_poznan}")
