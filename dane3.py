ca6fd97d7a0808b31f9f537055e38bfa9392c6b1
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-16 12:47:24 +0100

    refaktoring nazw

--- dane3.py
+++ dane3.py
@@ -1,6 +1,6 @@
 class CityAnalizer:
     def __init__(self, filename = 'dane.csv'):
-        self.citys = {}
+        self.cities = {}
         self.load_data(filename)
 
     def load_data(self, filename):
@@ -13,30 +13,30 @@ class CityAnalizer:
         for row in data:
             city = row[4]
             height = int(row[2])
-            if city in self.citys:
-                self.citys[city]['total'] += height
-                self.citys[city]['number'] += 1
+            if city in self.cities:
+                self.cities[city]['total'] += height
+                self.cities[city]['number'] += 1
             else:
-                self.citys[city] = {'total': height, 'number': 1}
+                self.cities[city] = {'total': height, 'number': 1}
 
     def show_height(self, city='', x=2):
         if city:
-            if city in self.citys:
-                data = self.citys[city]
+            if city in self.cities:
+                data = self.cities[city]
                 avarage = data['total'] / data['number']
                 print(f"Średni wzrost w mieście {city:<8.8} wynosi: {avarage:.{x}f}")
             else:
                 print(f"Nie mam takiego miasta jak {city} w bazie.")
         else:
-            for city, data in self.citys.items():
+            for city, data in self.cities.items():
                 avarage = data['total'] / data['number']
                 print(f"Średni wzrost w mieście {city:<8.8} wynosi: {avarage:.{x}f}")
 
-    def show_citys(self):
+    def show_cities(self):
         print("Dostępne miasta w bazie: ", end="")
         #for city in self.citys.keys():
         #    print(city, end=", ")
-        list = ", ".join(self.citys.keys())
+        list = ", ".join(self.cities.keys())
         print(list)
 
 #city = CityAnalizer()
@@ -46,17 +46,17 @@ class CityAnalizer:
 #city.show_citys()
 
 def main():
-    city = CityAnalizer()
+    cities_data = CityAnalizer()
 
     while True:
-        city.show_citys()
-        chose = input("Podaj nazwę miasta (lub 'exit' aby wyjść): ").capitalize()
+        cities_data.show_cities()
+        chose = input("Podaj nazwę miasta (lub 'exit' aby wyjść, 'load' aby wczytać ponownie dane): ").capitalize()
 
         if chose.lower() == 'exit':
             print("Dziękuję za współpracę!")
             break
 
-        city.show_height(chose)
+        cities_data.show_height(chose)
 
 
 if __name__ == "__main__":
