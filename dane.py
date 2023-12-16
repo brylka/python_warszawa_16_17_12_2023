6a5b89fa3a593959df1352f5d17584f29247df86
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-16 09:02:33 +0100

    wczytanie danych do listy

--- dane.py
+++ dane.py
@@ -2,5 +2,10 @@ with open('dane.csv', 'r', encoding='utf-8') as file:
     first_line = file.readline()
     lines = file.readlines()
 
-print(first_line)
-print(lines)
+#print(first_line)
+#print(lines)
+
+data = [line.strip().split(',') for line in lines]
+
+#print(data[1:])
+print(data)
