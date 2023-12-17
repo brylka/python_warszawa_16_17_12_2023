8c7c2a70a7fc95c6ddacf614460d6214b71decfd
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-17 08:57:32 +0100

    pandas v2

--- dane5.py
+++ dane5.py
@@ -3,7 +3,9 @@ import pandas as pd
 class CityAnalizer:
     def __init__(self, filename='dane.csv'):
         self.dataframe = pd.read_csv(filename, sep=',', encoding='utf-8')
-        print(self.dataframe)
-
+        #print(self.dataframe)
+        self.dataframe.columns = ['id', 'age', 'height', 'weight', 'city']
+        self.city_avarages = self.dataframe.groupby('city')['height'].mean()
+        print(self.city_avarages)
 
 coty = CityAnalizer()
