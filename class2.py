d8916dff82fac92a611897fc8b42d5c45448e1b0
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-17 14:33:15 +0100

    class v3

--- class2.py
+++ class2.py
@@ -9,9 +9,13 @@ class Person:
     def __str__(self):
         return f"Obiekt z klasy Person o nazwie: {self.name}, wieku: {self.age}, numerze seryjnym: {self.serial_number}"
 
+    def __del__(self):
+        print("Ubili mnie!")
 
 bartosz = Person("Bartosz",44)
 #bartosz.hello()
 
 print(bartosz)
+
+del bartosz
 
