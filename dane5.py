9eb0b73ef56a9cb2a6f3490474d80468fbac0c96
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-17 10:03:29 +0100

    pandas v7

--- dane5.py
+++ dane5.py
@@ -7,7 +7,7 @@ class CityAnalizer:
         self.dataframe = pd.read_csv(filename, sep=',', encoding='utf-8')
         #print(self.dataframe)
         self.dataframe.columns = ['id', 'age', 'height', 'weight', 'city']
-        self.city_avarages = self.dataframe.groupby('city')[['height','age']].mean()
+        self.city_avarages = self.dataframe.groupby('city')[['height','age', 'weight']].mean()
         #print(self.city_avarages)
 
     def plot(self):
@@ -20,7 +20,11 @@ class CityAnalizer:
             height = i.get_height()
             plt.annotate(f"{height:.2f}",
                          (i.get_x()+i.get_width() / 2, height),
-                          textcoords="offset points", xytext=(0,5), ha='center')
+                         textcoords="offset points",
+                         xytext=(1,-40),
+                         ha='center',
+                         rotation = 90,
+                         color="white")
         #for i, value in enumerate(self.city_avarages):
         #    plt.annotate(f"{value:.2f}", (i, value), textcoords="offset points", xytext=(0,5), ha='center')
         plt.show()
