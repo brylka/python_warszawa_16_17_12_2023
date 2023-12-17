51df5147b0ec9295ce0993b0bd412aeb7d8a9fc3
Bartosz Bryniarski <brylka@jelenia.com>
2023-12-17 09:51:23 +0100

    pandas v6

--- dane5.py
+++ dane5.py
@@ -7,17 +7,22 @@ class CityAnalizer:
         self.dataframe = pd.read_csv(filename, sep=',', encoding='utf-8')
         #print(self.dataframe)
         self.dataframe.columns = ['id', 'age', 'height', 'weight', 'city']
-        self.city_avarages = self.dataframe.groupby('city')['height'].mean()
+        self.city_avarages = self.dataframe.groupby('city')[['height','age']].mean()
         #print(self.city_avarages)
 
     def plot(self):
-        self.city_avarages.plot(kind='bar', figsize=(10, 6))
+        ax = self.city_avarages.plot(kind='bar', figsize=(10, 6))
         plt.title('Średni wzrost w różnych miastach.')
         plt.xlabel('Miasto')
         plt.ylabel('Średni wzrost w miastach')
         plt.xticks(rotation=0)
-        for i, value in enumerate(self.city_avarages):
-            plt.annotate(f"{value:.2f}", (i, value), textcoords="offset points", xytext=(0,5), ha='center')
+        for i in ax.patches:
+            height = i.get_height()
+            plt.annotate(f"{height:.2f}",
+                         (i.get_x()+i.get_width() / 2, height),
+                          textcoords="offset points", xytext=(0,5), ha='center')
+        #for i, value in enumerate(self.city_avarages):
+        #    plt.annotate(f"{value:.2f}", (i, value), textcoords="offset points", xytext=(0,5), ha='center')
         plt.show()
 
 
